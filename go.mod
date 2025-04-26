720400d5f7bfed0b5f3e98eacc4e2b4a439d5c84
Admin <4info50@gmail.com>
2025-04-26 23:39:32 +0300

    test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module testEdme
+
+go 1.24
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	// Генерируем случайное число от 1 до 10
+	rand.Seed(time.Now().UnixNano())
+	secretNumber := rand.Intn(10) + 1
+
+	fmt.Println("Я загадал число от 1 до 10. Попробуй угадать!")
+
+	// Цикл для угадывания
+	for {
+		var guess int
+		fmt.Print("Твой вариант: ")
+		_, err := fmt.Scan(&guess)
+
+		// Проверяем, что ввели число
+		if err != nil {
+			fmt.Println("Ой! Нужно ввести число. Попробуй ещё раз.")
+			// Очищаем буфер ввода
+			var discard string
+			fmt.Scanln(&discard)
+			continue
+		}
+
+		// Проверяем, что число от 1 до 10
+		if guess < 1 || guess > 10 {
+			fmt.Println("Число должно быть от 1 до 10! Попробуй ещё раз.")
+			continue
+		}
+
+		// Проверяем угадал ли пользователь
+		if guess == secretNumber {
+			fmt.Printf("Ура! Ты угадал! Это было число %d.\n", secretNumber)
+			break
+		} else if guess < secretNumber {
+			fmt.Println("Моё число больше!")
+		} else {
+			fmt.Println("Моё число меньше!")
+		}
+	}
+}
